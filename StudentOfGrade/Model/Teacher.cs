94b4197332b9ee34bc85c4cd1b3a5f7eda3263cf
zHcSing <673669013@qq.com>
2016-04-25 17:16:02 +0800

    创建了entity模型

---
+++ StudentOfGrade/Model/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Model
+{
+  public  class Teacher
+    {
+      public int TeacherID { get; set; }
+      [MaxLength(10)]
+      public string Tname { get; set; }
+
+    }
+}
