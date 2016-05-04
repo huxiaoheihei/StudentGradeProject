ce9a1fb48ed49e319d34508b645f223d645ce435
zHcSing <673669013@qq.com>
2016-05-04 10:15:44 +0800

    更改

---
+++ StudentOfGrade/Model/StudentOfGradeContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+   public class StudentOfGradeContext:DbContext
+    {
+       public DbSet<Student> Students { get; set; }
+
+       public DbSet<Teacher> Teachers { get; set; }
+
+       public DbSet<Team> Teams { get; set; }
+
+       public DbSet<Course> Courses { get; set; }
+       
+       public DbSet<GradeItem> GradeItems { get; set; }
+
+       public DbSet<PersonalGrade> PersonalGrades { get; set; }
+
+       public DbSet<TeamGrade> TeamGrades { get; set; }
+    }
+}
