6e121c82aeddcdb4c6c584c67035620de8ee669a
zHcSing <673669013@qq.com>
2016-05-04 09:48:12 +0800

    添加了Code First生成数据库

---
+++ StudentOfGrade/Web.UI/Controllers/AddTeamController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.UI.Models;
+using Model;
+
+namespace Web.UI.Controllers
+{
+    public class AddTeamController : Controller
+    {
+        //
+        // GET: /AddTeam/
+        
+        public ActionResult Index()
+        {
+            ModelToView mv = new ModelToView();
+            mv.team.TeamName = "蛋炒饭";
+            StudentOfGradeContext context = new StudentOfGradeContext();
+            context.Teams.Add(mv.team);
+            return View();
+        }
+        
+     
+
+    }
+}
