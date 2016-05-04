e7ab0b79aaa34b72a4f841d8dc3bb082da351fe8
zHcSing <673669013@qq.com>
2016-05-04 10:43:18 +0800

    。。

---
+++ StudentOfGrade/Web.UI/Models/ModelToView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+
+namespace Web.UI.Models
+{
+    public class ModelToView
+    {
+        public ModelToView()
+        {
+            this.student = new Student();
+            this.teacher =new Teacher();
+            this.team = new Team();
+            this.teamGrade = new TeamGrade();
+            this.personalGrade = new PersonalGrade();
+            this.course = new Course();
+            this.gradeItem = new GradeItem();
+        }
+        public Course course { get; set; }
+        public Student student { get;set; }
+        public Teacher teacher { get; set; }
+        public Team team { get; set; }
+        public TeamGrade teamGrade { get; set; }
+        public PersonalGrade personalGrade { get; set; }
+        public GradeItem gradeItem { get; set; }
+    }
+}
